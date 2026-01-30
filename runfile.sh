afb4fb51233041dc37f00dd3857a6e3682cb6551
AHSchmA1 <ahschma1@SRV0R26.img.med.uni-tuebingen.de>
2026-01-30 11:45:02 +0100

    update

--- runfile.sh
+++
@@ -1,2 +0,0 @@
-Rscript --vanilla tsi.R --expData ~/Downloads/rna_tissue_consensus.tsv --entity pancreas --outPath tsiPanc.txt
-Rscript --vanilla tsiCell.R --expData ~/Downloads/rna_single_cell_type.tsv --entity /home/andi/Documents/tsi/entities.txt --outPath tsiPancCell.txt
